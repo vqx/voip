c6bc270d02cc161a4c67ca4d2f40d81dfc84379a
xiangzhifa <xpoony@163.com>
2019-01-07 14:31:03 +0800

    save

--- android/voip/app/src/main/java/git/xzf/voip/Audio.java
+++ android/voip/app/src/main/java/git/xzf/voip/Audio.java
@@ -1,51 +1,73 @@
 package git.xzf.voip;
 
 import android.media.AudioFormat;
+import android.media.AudioManager;
 import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.media.MediaRecorder;
 
 public class Audio {
     int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
     int BytesPerElement = 2; // 2 bytes in 16bit format
-    private static final int RECORDER_SAMPLERATE = 8000;
+    private static final int RECORDER_SAMPLERATE = 44100;
     private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
     private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
     AudioRecord record;
     TcpClient tcpClient;
+    AudioTrack audioTrack;
+    ByteQueue queue;
 
     public Audio() {
-        record = new AudioRecord(MediaRecorder.AudioSource.MIC,
-                RECORDER_SAMPLERATE, RECORDER_CHANNELS,
-                RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
-        tcpClient = new TcpClient(Config.Address, Config.Port);
+        queue = new ByteQueue();
+        record = new AudioRecord(MediaRecorder.AudioSource.DEFAULT,
+                RECORDER_SAMPLERATE,
+                RECORDER_CHANNELS,
+                RECORDER_AUDIO_ENCODING,
+                BufferElements2Rec * BytesPerElement);
+        audioTrack = new AudioTrack(
+                AudioManager.STREAM_VOICE_CALL,
+                RECORDER_SAMPLERATE,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                BufferElements2Rec * BytesPerElement,
+                AudioTrack.MODE_STREAM);
+        // tcpClient = new TcpClient(Config.Address, Config.Port);
     }
 
     boolean recording;
 
-    public void Record() {
+    public void Record() throws Exception {
         Log.P("enter");
-        tcpClient.Connect();
+        // tcpClient.Connect();
         record.startRecording();
         recording = true;
-        while (recording) {
-            try {
-                byte sData[] = new byte[BufferElements2Rec];
-                record.read(sData, 0, BufferElements2Rec);
-                tcpClient.Send(sData);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.P("start record thread ");
+                while (recording) {
+                    byte sData[] = new byte[BufferElements2Rec];
+                    record.read(sData, 0, BufferElements2Rec);
+                    queue.In(sData);
+                    //  tcpClient.Send(sData);
 
-//                for (int i = 0; i < BufferElements2Rec; i++) {
-//                    String str = "";
-//                    for (int j = 0; j < (int) sData[i]; j++) {
-//                        str += "-";
-//                    }
-//                    Log.P(str);
-//                }
-            } catch (Exception e) {
-                Log.P("error", e.toString());
-
+                }
             }
-        }
-        Log.P("out");
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.P("start play thread ");
+                while (recording) {
+                    byte[] tmp = queue.Out();
+                    if (tmp.length != 0) {
+                        audioTrack.write(tmp, 0, BufferElements2Rec);
+                        audioTrack.play();
+                    }
+                }
+            }
+        }).start();
+        Log.P("end run Thread");
     }
 
     public void StopRecord() {
---
+++ android/voip/app/src/main/java/git/xzf/voip/ByteQueue.java
@@ -0,0 +1,39 @@
+package git.xzf.voip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ByteQueue {
+    private Lock lock;
+    private List<byte[]> queue;
+
+    public ByteQueue() {
+        queue = new ArrayList<>();
+        lock = new Lock();
+    }
+
+    public void In(byte[] data) {
+        try {
+            lock.lock();
+            queue.add(data);
+            lock.unlock();
+        } catch (Exception e) {
+            Log.P("ByteQueue.In", e.toString());
+        }
+    }
+
+    public byte[] Out() {
+        if (queue.size() != 0) {
+            try {
+                lock.lock();
+                byte[] tmp = queue.get(0).clone();
+                queue.remove(0);
+                lock.unlock();
+                return tmp;
+            } catch (Exception e) {
+                Log.P("ByteQueue.Out", e.toString());
+            }
+        }
+        return new byte[0];
+    }
+}
